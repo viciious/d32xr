e1921d7571d7c8ebab4fbb50e1ea56569d43724c
Victor Luchits <vluchits@gmail.com>
2021-03-07 14:28:22 +0300

    Fix D_strncasecmp to be case-insensitive

--- d_main.c
+++ d_main.c
@@ -109,16 +109,21 @@ int D_strncasecmp (const char *s1, const char *s2, int len)
 {
 	while (*s1 && *s2)
 	{
-		if (*s1 != *s2)
-			return 1;
+		int c1 = *s1, c2 = *s2;
+
+		if (c1 >= 'A' && c1 <= 'Z') c1 += 'a' - 'A';
+		if (c2 >= 'A' && c2 <= 'Z') c2 += 'a' - 'A';
+
+		if (c1 != c2)
+			return c1 - c2;
+
 		s1++;
 		s2++;
+
 		if (!--len)
 			return 0;
 	}
-	if (*s1 != *s2)
-		return 1;
-	return 0;	
+	return *s1 - *s2;
 }
 
 
