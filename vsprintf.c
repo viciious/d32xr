da9d62ba9a5de616c8bb580abf0fd6882cf52b33
viciious <vluchits@gmail.com>
2021-07-20 13:18:28 +0300

    Do not use stdlib's vsnprintf to save on ROM space

--- vsprintf.c
+++ vsprintf.c
@@ -50,33 +50,120 @@ int mystrlen(const char *string)
    return rc;
 }
 
-//
-// CALICO: Replaced with call to C library function to eliminate non-portable
-// argument-passing idiom.
-//
-int D_vsnprintf(char *str, size_t nmax, const char *format, va_list ap)
+int D_vsnprintf(char* string, size_t nmax, const char* format, va_list argptr)
 {
-   int result;
+    int len, i, div, uselong;
+    int fieldsize;
+    unsigned long num;
+    long snum;
+    char padchar;
+    char* str;
+    char* origstring = string;
 
-   if(nmax < 1)
-   {
-      return 0;
-   }
+    while (*format)
+    {
+        if (*format != '%') *(string++) = *(format++);
+        else
+        {
+            format++;
 
-   // vsnprintf() in Windows (and possibly other OSes) doesn't always
-   // append a trailing \0. We have the responsibility of making this
-   // safe by writing into a buffer that is one byte shorter ourselves.
-   result = vsnprintf(str, nmax, format, ap);
+            /* set field pad character to 0 if necessary */
+            if (*format == '0')
+            {
+                padchar = '0';
+                format++;
+            }
+            else padchar = ' ';
 
-   // If truncated, change the final char in the buffer to a \0.
-   // In Windows, a negative result indicates a truncated buffer.
-   if(result < 0 || (size_t)result >= nmax)
-   {
-      str[nmax - 1] = '\0';
-      result = (int)(nmax - 1);
-   }
+            /* get the fieldwidth if any */
+            fieldsize = 0;
+            while (*format >= '0' && *format <= '9')
+                fieldsize = fieldsize * 10 + *(format++) - '0';
 
-   return result;
+            /* get rid of 'l' if present */
+            if (*format == 'l')
+            {
+                uselong = 1;
+                format++;
+            }
+            else uselong = 0;
+
+            div = 10;
+            if (*format == 'c')
+            {
+                *(string++) = va_arg(argptr, int);
+                format++;
+            }
+            else if (*format == 's')
+            {
+                str = (char*)va_arg(argptr, int);
+                len = mystrlen(str);
+                while (fieldsize-- > len) *(string++) = padchar; /* do field pad */
+                while (*str) *(string++) = *(str++); /* copy string */
+                format++;
+            }
+            else
+            {
+                if (*format == 'o') /* octal */
+                {
+                    div = 8;
+                    if (uselong)
+                        num = va_arg(argptr, int);
+                    else
+                        num = va_arg(argptr, int);
+                    /*	  printf("o=0%o\n", num); */
+                }
+                else if (*format == 'x' || *format == 'X' || *format == 'p' || *format == 'P')  /* hex */
+                {
+                    div = 16;
+                    if (uselong)
+                        num = va_arg(argptr, int);
+                    else
+                        num = va_arg(argptr, int);
+                    /*	  printf("x=%x\n", num); */
+                }
+                else if (*format == 'i' || *format == 'd' || *format == 'u') /* decimal */
+                {
+                    div = 10;
+                    if (uselong)
+                        snum = va_arg(argptr, int);
+                    else
+                        snum = va_arg(argptr, int);
+                    if (snum < 0 && *format != 'u') /* handle negative %i or %d */
+                    {
+                        *(string++) = '-';
+                        num = -snum;
+                        if (fieldsize) fieldsize--;
+                    }
+                    else num = snum;
+                }
+                else return -1; /* unrecognized format specifier */
+
+                /* print any decimal or hex integer */
+                len = 0;
+                while (num || fieldsize || !len)
+                {
+                    for (i = len; i; i--) string[i] = string[i - 1]; /* shift right */
+                    if (len && fieldsize && !num) *string = padchar; /* pad out */
+                    else
+                    {
+                        /* put in a hex or decimal digit */
+                        *string = num % div;
+                        *string += *string > 9 ? 'A' - 10 : '0';
+                        /*	    printf("d = %c\n", *string); */
+                        num /= div;
+                    }
+                    len++;
+                    if (fieldsize) fieldsize--;
+                }
+                string += len;
+                format++;
+            }
+        }
+    }
+    *string = 0;
+
+    return origstring - string;
 }
 
 // EOF
