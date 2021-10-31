9b880d622c4477209d80d552029e3bff49195ebf
viciious <vluchits@gmail.com>
2021-10-31 09:56:55 +0300

    Update ROM version string

--- crt0.s
+++ crt0.s
@@ -27,7 +27,7 @@
 
 ! Standard MegaDrive ROM header at 0x100
 
-        .ascii  "SEGA 32X        "
+        .ascii  "SEGA GENESIS    "
         .ascii  "                "
         .ascii  "DOOM 32X Resurre"
         .ascii  "ction           "
@@ -35,7 +35,7 @@
         .ascii  "DOOM 32X Resurre"
         .ascii  "ction           "
         .ascii  "                "
-        .ascii  "GM 20211026-00"
+        .ascii  "GM 20211031-00"
         .word   0x0000
         .ascii  "J6              "
         .long   0x00000000,0x003FFFFF   /* ROM start, end */
