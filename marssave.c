618bece9157e8e3c07d520ec672b3f4f17928a00
Victor <vluchits@gmail.com>
2021-11-16 12:36:43 +0300

    Update marssave.c

--- marssave.c
+++ marssave.c
@@ -3,7 +3,7 @@
 
   The MIT License (MIT)
 
-  Copyright (c) 2021 Victor Luchits, Derek John Evans, id Software and ZeniMax Media
+  Copyright (c) 2021 Victor Luchits
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
