69b32ccaf990e72b590d6216b8225c9456deb055
viciious <vluchits@gmail.com>
2022-02-10 12:59:27 +0300

    Fix MEDPro detection

--- src-md/crt0.s
+++ src-md/crt0.s
@@ -247,7 +247,7 @@ do_main:
 
 | detect the Mega EverDrive
         tst.w   megasd_ok
-        beq     main_loop_start
+        bne     main_loop_start
 
         move.w  #0x2700,sr          /* disable ints */
         move.b  #1,0xA15107         /* set RV */
