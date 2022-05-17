056d856b7419f9933532b599c572ad4a3677a342
viciious <vluchits@gmail.com>
2022-05-17 11:47:46 +0400

    Add ATTR_DATA_OPTIMIZE_NONE

--- doomdef.h
+++ doomdef.h
@@ -43,10 +43,12 @@ typedef unsigned short inpixel_t;
 #endif
 
 #ifdef MARS
+#define ATTR_DATA_OPTIMIZE_NONE __attribute__((section(".data"), aligned(16), optimize("O1")))
 #define ATTR_DATA_CACHE_ALIGN __attribute__((section(".data"), aligned(16), optimize("Os")))
 #define ATTR_OPTIMIZE_SIZE __attribute__((optimize("Os")))
 #define ATTR_OPTIMIZE_EXTREME __attribute__((optimize("O3", "no-align-loops", "no-align-functions", "no-align-jumps", "no-align-labels")))
 #else
+#define ATTR_DATA_OPTIMIZE_NONE
 #define ATTR_DATA_CACHE_ALIGN
 #define ATTR_OPTIMIZE_SIZE
 #define ATTR_OPTIMIZE_EXTREME
