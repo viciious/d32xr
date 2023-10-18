49b818e8315e7882006778cdb603ed3e5faccb53
viciious <vluchits@gmail.com>
2023-10-18 15:20:49 +0300

    Working CD file cache

--- marsnew.c
+++ marsnew.c
@@ -1214,3 +1214,13 @@ int I_ReadCDFile(int length)
 {
 	return Mars_ReadCDFile(length);
 }
+
+void I_SetCDSetFileCache(int length)
+{
+	Mars_MCDSetFileCache(length);
+}
+
+void *I_GetCDFileCache(int length)
+{
+	return Mars_MCDGetFileCache(length);
+}
