fb36a151067192d54827e0545c779611e58bde7a
viciious <vluchits@gmail.com>
2023-11-26 11:10:36 +0300

    Tweak fuzz colormap

--- r_main.c
+++ r_main.c
@@ -638,7 +638,7 @@ static void R_Setup (int displayplayer, visplane_t *visplanes_,
 	if (vd.fixedcolormap == INVERSECOLORMAP)
 		vd.fuzzcolormap = INVERSECOLORMAP;
 	else
-		vd.fuzzcolormap = 6 * 256;
+		vd.fuzzcolormap = 12 * 256;
 #endif
 
 	vd.visplanes = visplanes_;
