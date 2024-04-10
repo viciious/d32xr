697bd54422b3dc0914b6919e327cb66aa6763dc0
viciious <vluchits@gmail.com>
2024-04-10 20:56:47 +0300

    Split the low res drawing routines into sh2_drawlow.s

--- sh2_drawlow.s
+++ sh2_drawlow.s
@@ -7,13 +7,14 @@
 
 ! Draw a vertical column of pixels from a projected wall texture.
 ! Source is the top of the column to scale.
+! Low detail (doubl-wide pixels) mode.
 !
-!void I_DrawColumn(int dc_x, int dc_yl, int dc_yh, int light, fixed_t frac,
+!void I_DrawColumnLow(int dc_x, int dc_yl, int dc_yh, int light, fixed_t frac,
 !                  fixed_t fracstep, inpixel_t *dc_source, int dc_texheight)
 
         .align  4
-        .global _I_DrawColumnA
-_I_DrawColumnA:
+        .global _I_DrawColumnLowA
+_I_DrawColumnLowA:
 	add	#1,r6
 
 0:
@@ -28,10 +29,12 @@ _I_DrawColumnA:
         mov.l   r8,@-r15
         mov.l   r9,@-r15
         mov.l   @(DOOMTLS_COLORMAP, gbr),r0
+        add     r7,r7
         add     r0,r7           /* dc_colormap = colormap + light */
         mov.l   draw_fb,r8
         mov.l   @r8,r8          /* frame buffer start */
-        add     r4,r8           /* fb += dc_x */
+        add     r4,r8
+        add     r4,r8           /* fb += dc_x*2 */
         shll8   r5
         add     r5,r8
         shlr2   r5
@@ -42,34 +45,35 @@ _I_DrawColumnA:
         mov.l   @(20,r15),r4
         mov.l   draw_width,r1
         add     #-1,r4          /* heightmask = texheight - 1 */
-
         swap.w  r2,r0           /* (frac >> 16) */
         and     r4,r0           /* (frac >> 16) & heightmask */
 
         /* test if count & 1 */
         shlr    r6
         movt    r9              /* 1 if count was odd */
-        bt/s    do_col_loop_1px
+        bt/s    do_col_loop_low_1px
         add     r9,r6
 
         .p2alignw 2, 0x0009
-do_col_loop:
+do_col_loop_low:
         mov.b   @(r0,r5),r0     /* pix = dc_source[(frac >> 16) & heightmask] */
+        add     r0,r0
+        mov.w   @(r0,r7),r9     /* dpix = dc_colormap[pix] */
         add     r3,r2           /* frac += fracstep */
-        mov.b   @(r0,r7),r9     /* dpix = dc_colormap[pix] */
         swap.w  r2,r0           /* (frac >> 16) */
-        mov.b   r9,@r8          /* *fb = dpix */
+        and     r4,r0           /* (frac >> 16) & heightmask */
+        mov.w   r9,@r8          /* *fb = dpix */
         add     r1,r8           /* fb += SCREENWIDTH */
-        and     r4,r0           /* (frac >> 16) & heightmask */
-do_col_loop_1px:
-        dt      r6              /* count-- */
+do_col_loop_low_1px:
         mov.b   @(r0,r5),r0     /* pix = dc_source[(frac >> 16) & heightmask] */
+        add     r0,r0
+        mov.w   @(r0,r7),r9     /* dpix = dc_colormap[pix] */
         add     r3,r2           /* frac += fracstep */
-        mov.b   @(r0,r7),r9     /* dpix = dc_colormap[pix] */
+        dt      r6              /* count-- */
         swap.w  r2,r0           /* (frac >> 16) */
+        mov.w   r9,@r8          /* *fb = dpix */
         and     r4,r0           /* (frac >> 16) & heightmask */
-        mov.b   r9,@r8          /* *fb = dpix */
-        bf/s    do_col_loop
+        bf/s    do_col_loop_low
         add     r1,r8           /* fb += SCREENWIDTH */
 
         mov.l   @r15+,r9
@@ -78,13 +82,14 @@ do_col_loop_1px:
 
 ! Draw a vertical column of pixels from a projected wall texture.
 ! Non-power of 2 texture height.
+! Low detail (doubl-wide pixels) mode.
 !
-!void I_DrawColumnNPO2(int dc_x, int dc_yl, int dc_yh, int light, fixed_t frac,
+!void I_DrawColumnNPO2Low(int dc_x, int dc_yl, int dc_yh, int light, fixed_t frac,
 !                      fixed_t fracstep, inpixel_t *dc_source, int dc_texheight)
 
         .align  4
-        .global _I_DrawColumnNPo2A
-_I_DrawColumnNPo2A:
+        .global _I_DrawColumnNPo2LowA
+_I_DrawColumnNPo2LowA:
 	add	#1,r6
 
 0:
@@ -98,10 +103,12 @@ _I_DrawColumnNPo2A:
 1:
         mov.l   r8,@-r15
         mov.l   @(DOOMTLS_COLORMAP, gbr),r0
+        add     r7,r7
         add     r0,r7           /* dc_colormap = colormap + light */
         mov.l   draw_fb,r8
         mov.l   @r8,r8          /* frame buffer start */
-        add     r4,r8           /* fb += dc_x */
+        add     r4,r8
+        add     r4,r8           /* fb += dc_x*2 */
         shll8   r5
         add     r5,r8
         shlr2   r5
@@ -134,36 +141,36 @@ _I_DrawColumnNPo2A:
         sub     r1,r8           /* fb -= SCREENWIDTH */
 
         .p2alignw 2, 0x0009
-do_cnp_loop:
+do_cnp_loop_low:
         mov     r2,r0
         shlr16  r0              /* frac >> 16 */
         mov.b   @(r0,r5),r0     /* pix = dc_source[frac >> 16] */
+        add     r1,r8           /* fb += SCREENWIDTH */
         add     r3,r2           /* frac += fracstep */
         cmp/ge  r4,r2
+        add     r0,r0
         bf/s    1f
-        mov.b   @(r0,r7),r0     /* dpix = dc_colormap[pix] */
+        mov.w   @(r0,r7),r0     /* dpix = dc_colormap[pix] */
         /* if (frac >= heightmask) */
         sub     r4,r2           /* frac -= heightmask */
 1:
-        add     r1,r8           /* fb += SCREENWIDTH */
         dt      r6              /* count-- */
-        bf/s    do_cnp_loop
-        mov.b   r0,@r8          /* *fb = dpix */
+        bf/s    do_cnp_loop_low
+        mov.w   r0,@r8          /* *fb = dpix */
 
         rts
         mov.l   @r15+,r8
-
 
 ! Draw a vertical column of distorted background pixels.
 ! Source is the top of the column to scale.
 ! Low detail (doubl-wide pixels) mode.
 !
-!void I_DrawFuzzColumn(int dc_x, int dc_yl, int dc_yh, int light, fixed_t frac,
+!void I_DrawFuzzColumnLow(int dc_x, int dc_yl, int dc_yh, int light, fixed_t frac,
 !       fixed_t fracstep, inpixel_t *dc_source, int dc_texheight)
 
         .align  4
-        .global _I_DrawFuzzColumnA
-_I_DrawFuzzColumnA:
+        .global _I_DrawFuzzColumnLowA
+_I_DrawFuzzColumnLowA:
         mov     #0,r0
         cmp/eq  r0,r5
         bf      0f
@@ -188,10 +195,12 @@ _I_DrawFuzzColumnA:
 3:
         mov.l   @(DOOMTLS_COLORMAP, gbr),r0
         mov.l   r8,@-r15
+        add     r7,r7
         add     r0,r7           /* dc_colormap = colormap + light */
         mov.l   draw_fb,r8
         mov.l   @r8,r8          /* frame buffer start */
-        add     r4,r8           /* fb += dc_x */
+        add     r4,r8
+        add     r4,r8           /* fb += dc_x*2 */
         shll8   r5
         add     r5,r8
         shlr2   r5
@@ -204,16 +213,17 @@ _I_DrawFuzzColumnA:
         and     #126,r0         /* fuzzpos &= FUZZMASK */
 
         .p2alignw 2, 0x0009
-do_fuzz_col_loop:
+do_fuzz_col_loop_low:
         mov.w   @(r0,r5),r0     /* pix = fuzztable[fuzzpos] */
-        add     #2,r3
-        mov.b   @(r0,r8),r0     /* pix = dest[pix] */
         dt      r6              /* count-- */
-        mov.b   @(r0,r7),r4     /* dpix = dc_colormap[pix] */
+        mov.b   @(r0,r8),r0     /* pix = dest[pix] */
+        add     r0,r0
+        mov.w   @(r0,r7),r4     /* dpix = dc_colormap[pix] */
+        add     #2,r3
         mov     r3,r0
-        mov.b   r4,@r8          /* *fb = dpix */
         and     #126,r0         /* fuzzpos &= FUZZMASK */
-        bf/s    do_fuzz_col_loop
+        mov.w   r4,@r8          /* *fb = dpix */
+        bf/s    do_fuzz_col_loop_low
         add     r1,r8           /* fb += SCREENWIDTH */
 
         shlr    r3
@@ -223,14 +233,15 @@ do_fuzz_col_loop:
         mov.l   @r15+,r8
 
 ! Draw a horizontal row of pixels from a projected flat (floor/ceiling) texture.
+! Low detail (doubl-wide pixels) mode.
 !
-!void I_DrawSpan(int ds_y, int ds_x1, int ds_x2, int light, fixed_t ds_xfrac,
+!void I_DrawSpanLow(int ds_y, int ds_x1, int ds_x2, int light, fixed_t ds_xfrac,
 !                fixed_t ds_yfrac, fixed_t ds_xstep, fixed_t ds_ystep,
 !                inpixel_t *ds_source, int ds_height)
 
         .align  4
-        .global _I_DrawSpanA
-_I_DrawSpanA:
+        .global _I_DrawSpanLowA
+_I_DrawSpanLowA:
 	add	#1,r6
 
 0:
@@ -241,7 +252,6 @@ _I_DrawSpanA:
         /* dc_yl >= dc_yh, exit */
         rts
         nop
-
 1:
         mov.l   r8,@-r15
         mov.l   r9,@-r15
@@ -249,10 +259,12 @@ _I_DrawSpanA:
         mov.l   r11,@-r15
         mov.l   r12,@-r15
         mov.l   @(DOOMTLS_COLORMAP, gbr),r0
+        add     r7,r7
         add     r0,r7           /* ds_colormap = colormap + light */
         mov.l   draw_fb,r8
         mov.l   @r8,r8          /* frame buffer start */
         add     r5,r8
+        add     r5,r8           /* fb += ds_x1*2 */
         shll8   r4
         add     r4,r8
         shlr2   r4
@@ -279,37 +291,40 @@ _I_DrawSpanA:
         /* test if count & 1 */
         shlr    r6
         movt    r1              /* 1 if count was odd */
-        bt/s    do_span_loop_1px
+        bt/s    do_span_low_loop_1px
         add     r1,r6
 
         .p2alignw 2, 0x0009
-do_span_loop:
+do_span_low_loop:
         mov.b   @(r0,r9),r0     /* pix = ds_source[spot] */
         add     r3,r2           /* xfrac += xstep */
         add     r5,r4           /* yfrac += ystep */
+        add     r0,r0
+        mov.w   @(r0,r7),r10    /* dpix = ds_colormap[pix] */
         swap.w  r4,r1           /* (yfrac >> 16) */
-        mov.b   @(r0,r7),r10    /* dpix = ds_colormap[pix] */
         and     r11,r1          /* (yfrac >> 16) & 63*64 */
         swap.w  r2,r0           /* (xfrac >> 16) */
         and     r12,r0          /* (xfrac >> 16) & 63 */
-        mov.b   r10,@r8         /* *fb = dpix */
         or      r1,r0           /* spot = ((yfrac >> 16) & *64) | ((xfrac >> 16) & 63) */
-        add     #1,r8           /* fb++ */
-do_span_loop_1px:
-        add     r3,r2           /* xfrac += xstep */
+        mov.w   r10,@r8         /* *fb = dpix */
+        add     #2,r8           /* fb++ */
+
+do_span_low_loop_1px:
         mov.b   @(r0,r9),r0     /* pix = ds_source[spot] */
+        add     r3,r2           /* xfrac += xstep */
         add     r5,r4           /* yfrac += ystep */
+        add     r0,r0
+        mov.w   @(r0,r7),r10    /* dpix = ds_colormap[pix] */
         swap.w  r4,r1           /* (yfrac >> 16) */
         and     r11,r1          /* (yfrac >> 16) & 63*64 */
-        mov.b   @(r0,r7),r10    /* dpix = ds_colormap[pix] */
         swap.w  r2,r0           /* (xfrac >> 16) */
         and     r12,r0          /* (xfrac >> 16) & 63 */
         or      r1,r0           /* spot = ((yfrac >> 16) & *64) | ((xfrac >> 16) & 63) */
-        mov.b   r10,@r8         /* *fb = dpix */
+        mov.w   r10,@r8         /* *fb = dpix */
 
         dt      r6              /* count-- */
-        bf/s    do_span_loop
-        add     #1,r8           /* fb++ */
+        bf/s    do_span_low_loop
+        add     #2,r8           /* fb++ */
 
         mov.l   @r15+,r12
         mov.l   @r15+,r11
