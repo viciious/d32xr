c50bc3718cba862a51ce94cc7e6726e2f1a93ffe
viciious <vluchits@gmail.com>
2024-07-11 00:59:56 +0300

    A very minor speedup

--- r_phase2.c
+++ r_phase2.c
@@ -326,7 +326,7 @@ void Mars_Sec_R_WallPrep(void)
         // check if master CPU finished exec'ing R_BSP()
         if (nextsegs == -2)
         {
-            MARS_SYS_COMM6 = 0xff00 | (last - first);
+            MARS_SYS_COMM6 = 0xfe00 | (last - first);
             Mars_ClearCacheLine(&vd->lastwallcmd);
             verylast = vd->lastwallcmd;
             last = verylast;
