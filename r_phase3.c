a6754a0676d3b6212c21a01234867a91e9965e36
SSNTails <github@spaddlewit.com>
2024-07-10 21:05:09 +0000

    cull ring draw distance a bit more

--- r_phase3.c
+++ r_phase3.c
@@ -45,7 +45,7 @@ static void R_PrepMobj(mobj_t *thing)
    if(tz < MINZ)
       return;
 
-   if (tz > 3072*FRACUNIT) // Cull draw distance
+   if (tz > 2048*FRACUNIT) // Cull draw distance
       return;
 
    gxt = FixedMul(tr_x, vd.viewsin);
