68174976fe7d7f6ee7062e133f4847848cd1db69
viciious <vluchits@gmail.com>
2024-10-08 20:56:10 +0300

    Bump version to v3.2

--- crt0.s
+++ crt0.s
@@ -30,12 +30,12 @@
         .ascii  "SEGA SSF        "
         .ascii  "                "
         .ascii  "DOOM 32X Resurre"
-        .ascii  "ction v3.1      "
+        .ascii  "ction v3.2      "
         .ascii  "                "
         .ascii  "DOOM 32X Resurre"
-        .ascii  "ction v3.1      "
+        .ascii  "ction v3.2      "
         .ascii  "                "
-        .ascii  "GM 20230824-00"
+        .ascii  "GM 20241008-00"
         .word   0x0000
         .ascii  "J6CM            "
         .long   0x00000000,0x004FFFFF   /* ROM start, end */
