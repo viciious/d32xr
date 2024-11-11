540acb6177dfc140f008c3f847e8e6ec4a1c3a2a
viciious <vluchits@gmail.com>
2024-11-11 19:26:13 +0300

    Reduce S_MEMBANK_SIZE to make room for more code

--- src-md/cd/s_main.c
+++ src-md/cd/s_main.c
@@ -6,7 +6,7 @@
 #include "s_cd.h"
 #include "s_spcm.h"
 
-#define S_MEMBANK_SIZE 462*1024
+#define S_MEMBANK_SIZE 460*1024
 #define S_MEMBANK_PTR s_membank
 static uint8_t s_membank[S_MEMBANK_SIZE];
 
